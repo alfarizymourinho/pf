6f226d4899713a95878fde4a1bfeb1ff19ece21c
alfarizymourinho <alfarizymourinho@gmail.com>
2023-07-29 11:44:11 +0700

    Website Portofolio Gua

--- portofolio/src/Portfolio.vue
+++ portofolio/src/Portfolio.vue
@@ -151,7 +151,7 @@ h1 {font-size:1.5em;margin-bottom:55px;}
       </template>
     </Navbar>
    
-<section class="bg-center bg-no-repeat bg-[url('img/three.jpg')] bg-gray-700 bg-blend-multiply bg-cover my-5">
+<section class="bg-center bg-no-repeat bg-[url('/img/three.jpg')] bg-gray-700 bg-blend-multiply bg-cover my-5">
     <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
         <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Kau mengenalku, maka kau membutuhkanku.</h1>
         <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Violet Evergarden is the best</p>
